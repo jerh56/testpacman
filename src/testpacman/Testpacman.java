955c072083382577b1c1fb1f054b7189eb923069
dianamaradi <>
2018-03-13 09:57:15 -0600

    Hola kpaza

--- src/testpacman/Testpacman.java
+++ src/testpacman/Testpacman.java
@@ -15,7 +15,7 @@ public class Testpacman {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("kpez");
+        System.out.println("kpez<");
     }
     
 }
